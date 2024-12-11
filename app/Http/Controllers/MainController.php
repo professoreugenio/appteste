874fe0513a12447528d49336e31602fe31fdde7a
professoreugenio <professoreugeniomls@gmail.com>
2024-12-10 22:47:05 -0300

    Atualizações Controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MainController
+class MainController extends Controller
 {
     public function home(){
         return view('home');
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/',[MainController::class, 'home'])->name('home');
 Route::get('/home',[MainController::class, 'home'])->name('home');
 Route::get('/sobre',[MainController::class, 'sobre'])->name('sobre');
 Route::get('/contato',[MainController::class, 'contato'])->name('contato');
